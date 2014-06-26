761a132865910b5811c4b02374aa3f5a8d9bde47
jeffzha <726144@qq.com>
2014-06-26 17:25:06 +0800

    Add update POI action

---
+++ src/com/winjune/wips/manager/poi/controller/UpdatePOIAction.java
@@ -0,0 +1,82 @@
+/*
+ * $Id: UpdatePoiAction.java 1400220 2012-10-19 18:49:39Z jogep $
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package com.winjune.wips.manager.poi.controller;
+
+
+import com.opensymphony.xwork2.ActionSupport;
+import com.opensymphony.xwork2.ModelDriven;
+import com.winjune.wips.manager.poi.model.entity.POI;
+import com.winjune.wips.manager.poi.model.entitymanager.POIRepositoryHibernate;
+
+public class UpdatePOIAction extends ActionSupport implements ModelDriven<POI>{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -8722649087770960911L;
+	
+	private POI poi = new POI();
+
+	public String input() throws Exception {
+
+		POI currentPoi = POIRepositoryHibernate.findPoi(poi.getMapId(),poi.getPlaceX(),poi.getPlaceY());
+		if (currentPoi != null)
+		{
+			currentPoi.setAlpha(poi.getAlpha());
+			currentPoi.setAudioUrl(poi.getAudioUrl());
+			currentPoi.setDetailedDesc(poi.getDetailedDesc());
+			currentPoi.setGeneralDesc(poi.getGeneralDesc());
+			currentPoi.setHallId(poi.getHallId());
+			currentPoi.setIconUrl(poi.getIconUrl());
+			currentPoi.setLabel(poi.getLabel());
+			currentPoi.setMaxZoomFactor(poi.getMaxZoomFactor());
+			currentPoi.setMinZoomFactor(poi.getMinZoomFactor());
+			currentPoi.setNeareastNaviNode(poi.getNeareastNaviNode());
+			currentPoi.setPicUrl(poi.getPicUrl());
+			currentPoi.setReachable(poi.getReachable());
+			currentPoi.setReadable(poi.getReadable());
+			currentPoi.setRotation(poi.getRotation());
+			currentPoi.setScale(poi.getScale());
+			currentPoi.setShareble(poi.getShareble());
+			currentPoi.setTtsNo(poi.getTtsNo());
+			currentPoi.setType(poi.getType());
+			currentPoi.setWebUrl(poi.getWebUrl());
+
+			POIRepositoryHibernate.savePOI(currentPoi);
+		}
+
+		return SUCCESS;
+	}
+
+	public POI getPoi() {
+		return poi;
+	}
+
+	public void setPoi(POI poi) {
+		this.poi = poi;
+	}
+
+	@Override
+	public POI getModel() {
+		// TODO Auto-generated method stub
+		return poi;
+	}
+}
